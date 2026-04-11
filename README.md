4936362b0866e86e49b1bf68cc95054c720c1b11
r02092 <87572333+r02092@users.noreply.github.com>
2026-04-11 23:00:53 +0900

    鉄道データを変換するスクリプトを作成

---
+++ README.md
@@ -0,0 +1,24 @@
+# ARを用いた高知県の観光を支援するシステムの試作
+
+[ソフトウェア工学で開発したシステム「SceneTrip」](https://github.com/r02092/se2025)において、ARを用いたコンテンツを提供するという案があったが、開発にかかる手間の都合により初期の段階から没になってしまったので、その代わりに作成
+
+## 機能
+
+- 特定の観光名所にカメラを向けると見られるARコンテンツ
+  - 土佐山田駅の駅名標
+  - 土佐日記御崎の泊碑（検出テストのみ）
+- 近隣のスポットの名前をカメラの映像上に重ねて表示
+- 鉄道沿線で使用するとその場所における発車標を表示
+  - 対応路線
+    - 四国旅客鉄道土讃線・大歩危駅〜窪川駅（大歩危駅自体は除く）
+    - 四国旅客鉄道予土線・川奥信号場〜北宇和島駅の両渡り線の部分
+    - 土佐くろしお鉄道全線
+  - 対応期間
+    - 2026年4月〜9月
+
+## 使用したものについて
+
+- スポット情報(`src/spots.json`)は、[Shareによって作成されたデータ](https://github.com/r02092/se2025/blob/main/database/seeders/DatabaseSeeder.php)を使用しています。
+- スポットの標高の推測値として、[国土地理院](https://www.gsi.go.jp/)により提供されている[地理院タイル](https://maps.gsi.go.jp/development/ichiran.html)を使用しています。
+- 四国旅客鉄道および土佐くろしお鉄道の時刻表データ(`src/timetable.json`)は、[鉄道運用Hub](https://unyohub.2pd.jp/)において、[いずみ茶](https://x.com/Izumicha_SB03)様により[クリエイティブ・コモンズ 表示 4.0 国際 ライセンス](https://creativecommons.org/licenses/by/4.0/deed.ja)の下で提供されています。
+- `src/setup.ts`では、[国土数値情報ダウンロードサイト](https://nlftp.mlit.go.jp/)より「[鉄道データ](https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N02-v3_1.html)」をダウンロードし、加工したデータを`src/generated/railway.json`に出力します。「[鉄道データ](https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N02-v3_1.html)」は[国土交通省](https://www.mlit.go.jp/)により[公共データ利用規約（第1.0版）](https://www.digital.go.jp/resources/open_data/public_data_license_v1.0)の下で提供されています。
---
+++ src/setup.ts
@@ -0,0 +1,131 @@
+import JSZip from "jszip";
+import fs from "fs";
+
+type Coord = [number, number];
+interface Line {
+	type: "Feature";
+	properties: {
+		N02_001: string;
+		N02_002: string;
+		N02_003: string;
+		N02_004: string;
+	};
+	geometry: {
+		type: "LineString";
+		coordinates: Coord[];
+	};
+}
+interface Branch {
+	coord: Coord;
+	prev: Coord[];
+}
+
+const allLine: Line[] = JSON.parse(
+	(await (
+		await JSZip.loadAsync(
+			await (
+				await fetch(
+					"https://nlftp.mlit.go.jp/ksj/gml/data/N02/N02-22/N02-22_GML.zip",
+				)
+			).arrayBuffer(),
+		)
+	)
+		.file("UTF-8/N02-22_RailroadSection.geojson")
+		?.async("string")) as string,
+).features;
+const equalCoord = (coord1: Coord, coord2: Coord) =>
+	coord1.every((e, i) => e === coord2[i]);
+const findByCoord = (line: Line[], coord: Coord, exclude: Coord[]) => {
+	return line.filter(
+		e =>
+			!exclude.some(f => equalCoord(e.geometry.coordinates[1], f)) &&
+			(equalCoord(e.geometry.coordinates[0], coord) ||
+				equalCoord(e.geometry.coordinates.at(-1) as Coord, coord)),
+	);
+};
+const output = [];
+const position = [{line: "tosa_nakamurasen", station: 0}];
+let nextCoord: Coord = [132.7132, 32.93251];
+let prev: Coord[] = [];
+const branchStack: Branch[] = [];
+let link: boolean = false;
+while (position[0].line !== "yodosen" || position[0].station) {
+	let paths = findByCoord(allLine, nextCoord, prev);
+	switch (position[0].line) {
+		case "tosa_nakamurasen":
+			if (position[0].station > 43)
+				position.push({line: "dosansen_susaki", station: 0});
+			if (paths.length - 1) {
+				const isBranch =
+					!branchStack.length &&
+					paths.some(e => e.properties.N02_003 === "予土線");
+				paths = paths.filter(e => e.properties.N02_003 === "中村線");
+				if (isBranch) {
+					branchStack.push({
+						coord: nextCoord,
+						prev: [...prev, paths[0].geometry.coordinates[1]],
+					});
+				}
+			}
+			break;
+		case "dosansen_susaki":
+			if (position[0].station > 53)
+				position.push({line: "dosansen", station: 0});
+			if (paths.length - 1)
+				paths = paths.filter(e => e.properties.N02_003 === "土讃線");
+			break;
+		case "dosansen":
+			if (paths.length === 2) {
+				link = equalCoord(
+					paths[0].geometry.coordinates[1],
+					paths[1].geometry.coordinates[1],
+				);
+				if (link && position.length === 1)
+					position.push({line: "tosa_gomensen", station: 0});
+				paths = paths.filter(e => e.properties.N02_003 === "土讃線");
+				if (!link) {
+					if (position.length - 1) position.pop();
+					position[0].station--;
+					if (paths.length - 1)
+						paths = paths.filter(e =>
+							equalCoord(e.geometry.coordinates[0], [133.69566, 33.64934]),
+						);
+					branchStack.push({
+						coord: nextCoord,
+						prev: [...prev, paths[0].geometry.coordinates[1]],
+					});
+				}
+			}
+	}
+	if (
+		!paths.length ||
+		(position[0].line === "dosansen" && position[0].station > 35)
+	) {
+		const branch = branchStack.pop() as Branch;
+		prev = branch.prev;
+		nextCoord = branch.coord;
+		if (paths.length) {
+			position[0] = {line: "tosa_gomensen", station: 1};
+			paths = [];
+		} else if (position[0].line === "tosa_gomensen") {
+			position[0] = {line: "yodosen", station: 37};
+			paths = [];
+		}
+	}
+	if (paths.length === 1) {
+		let coords = paths[0].geometry.coordinates;
+		prev = [coords[1]];
+		if (!equalCoord(coords[0], nextCoord)) coords = coords.toReversed();
+		for (let i = 0; i < coords.length - 1; i++) {
+			output.push({
+				coords: [coords[i], coords[i + 1]].map(e => e.toReversed()),
+				position: structuredClone(position),
+			});
+		}
+		if (!link && position.length - 1) position.shift();
+		position[0].station -= position[0].line !== "yodosen" ? -1 : 1;
+		if (link) position[1].station++;
+		nextCoord = coords.at(-1) as Coord;
+	}
+}
+fs.writeFile("src/generated/railway.json", JSON.stringify(output), () => {});
